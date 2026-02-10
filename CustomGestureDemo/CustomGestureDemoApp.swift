28dfc7436963ab59d4e765c89a60cc06aecaa48e
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2026-02-10 10:17:00 +0100

    Initial Commit

---
+++ CustomGestureDemo/CustomGestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGestureDemoApp.swift
+//  CustomGestureDemo
+//
+//  Created by Bekhruzjon Hakmirzaev on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomGestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
